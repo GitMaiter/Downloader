fa294212aa1d9b892c1753861e4a28d37aa7f8d1
GitMaiter <mcmaiter@gmail.com>
2019-08-14 02:33:37 +0300

    delete enum and more other changes

--- Downloader/Form1.Designer.cs
+++ Downloader/Form1.Designer.cs
@@ -28,8 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.DownloadButton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label = new System.Windows.Forms.Label();
@@ -37,37 +39,24 @@
             this.checkBox6 = new System.Windows.Forms.CheckBox();
             this.checkBox5 = new System.Windows.Forms.CheckBox();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.progressBar2 = new System.Windows.Forms.ProgressBar();
-            this.progressBar3 = new System.Windows.Forms.ProgressBar();
-            this.progressBar4 = new System.Windows.Forms.ProgressBar();
-            this.progressBar5 = new System.Windows.Forms.ProgressBar();
-            this.progressBar6 = new System.Windows.Forms.ProgressBar();
-            this.progressBar7 = new System.Windows.Forms.ProgressBar();
-            this.progressBar8 = new System.Windows.Forms.ProgressBar();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // DownloadButton
             // 
-            this.button1.Location = new System.Drawing.Point(353, 496);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 69);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Download";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.DownloadButton.Location = new System.Drawing.Point(235, 322);
+            this.DownloadButton.Margin = new System.Windows.Forms.Padding(2);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(129, 45);
+            this.DownloadButton.TabIndex = 1;
+            this.DownloadButton.Text = "Download";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel1.Controls.Add(this.progressBar8);
-            this.panel1.Controls.Add(this.progressBar7);
-            this.panel1.Controls.Add(this.progressBar6);
-            this.panel1.Controls.Add(this.progressBar5);
-            this.panel1.Controls.Add(this.progressBar4);
-            this.panel1.Controls.Add(this.progressBar3);
-            this.panel1.Controls.Add(this.progressBar2);
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.progressBar1);
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.button2);
@@ -75,17 +64,37 @@
             this.panel1.Controls.Add(this.comboBox);
             this.panel1.Controls.Add(this.checkBox6);
             this.panel1.Controls.Add(this.checkBox5);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(8, 8);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(891, 465);
+            this.panel1.Size = new System.Drawing.Size(594, 302);
             this.panel1.TabIndex = 2;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(392, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(191, 52);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(165, 14);
+            this.progressBar1.TabIndex = 9;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(588, 18);
+            this.button3.Location = new System.Drawing.Point(392, 12);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(235, 39);
+            this.button3.Size = new System.Drawing.Size(157, 25);
             this.button3.TabIndex = 8;
             this.button3.Text = "Select folder for download";
             this.button3.UseVisualStyleBackColor = true;
@@ -93,9 +102,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(768, 275);
+            this.button2.Location = new System.Drawing.Point(512, 179);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(84, 33);
+            this.button2.Size = new System.Drawing.Size(56, 21);
             this.button2.TabIndex = 7;
             this.button2.Text = "OK";
             this.button2.UseVisualStyleBackColor = true;
@@ -103,9 +113,10 @@
             // label
             // 
             this.label.AutoSize = true;
-            this.label.Location = new System.Drawing.Point(37, 74);
+            this.label.Location = new System.Drawing.Point(25, 48);
+            this.label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(0, 20);
+            this.label.Size = new System.Drawing.Size(0, 13);
             this.label.TabIndex = 6;
             this.label.Click += new System.EventHandler(this.Label1_Click);
             // 
@@ -114,19 +125,21 @@
             this.comboBox.DropDownHeight = 150;
             this.comboBox.FormattingEnabled = true;
             this.comboBox.IntegralHeight = false;
-            this.comboBox.ItemHeight = 20;
-            this.comboBox.Location = new System.Drawing.Point(41, 18);
+            this.comboBox.ItemHeight = 13;
+            this.comboBox.Location = new System.Drawing.Point(27, 12);
+            this.comboBox.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox.Name = "comboBox";
-            this.comboBox.Size = new System.Drawing.Size(494, 28);
+            this.comboBox.Size = new System.Drawing.Size(331, 21);
             this.comboBox.TabIndex = 5;
             this.comboBox.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
             // 
             // checkBox6
             // 
             this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(605, 172);
+            this.checkBox6.Location = new System.Drawing.Point(403, 112);
+            this.checkBox6.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(218, 24);
+            this.checkBox6.Size = new System.Drawing.Size(146, 17);
             this.checkBox6.TabIndex = 4;
             this.checkBox6.Text = "Delete images after install";
             this.checkBox6.UseVisualStyleBackColor = true;
@@ -134,9 +147,10 @@
             // checkBox5
             // 
             this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(605, 245);
+            this.checkBox5.Location = new System.Drawing.Point(403, 159);
+            this.checkBox5.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(186, 24);
+            this.checkBox5.Size = new System.Drawing.Size(126, 17);
             this.checkBox5.TabIndex = 3;
             this.checkBox5.Text = "Install after download";
             this.checkBox5.UseVisualStyleBackColor = true;
@@ -145,77 +159,14 @@
             // 
             this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.FolderBrowserDialog1_HelpRequest);
             // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(287, 80);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(248, 22);
-            this.progressBar1.TabIndex = 9;
-            this.progressBar1.Visible = false;
-            // 
-            // progressBar2
-            // 
-            this.progressBar2.Location = new System.Drawing.Point(287, 130);
-            this.progressBar2.Name = "progressBar2";
-            this.progressBar2.Size = new System.Drawing.Size(248, 22);
-            this.progressBar2.TabIndex = 10;
-            this.progressBar2.Visible = false;
-            // 
-            // progressBar3
-            // 
-            this.progressBar3.Location = new System.Drawing.Point(287, 180);
-            this.progressBar3.Name = "progressBar3";
-            this.progressBar3.Size = new System.Drawing.Size(248, 22);
-            this.progressBar3.TabIndex = 11;
-            this.progressBar3.Visible = false;
-            // 
-            // progressBar4
-            // 
-            this.progressBar4.Location = new System.Drawing.Point(287, 230);
-            this.progressBar4.Name = "progressBar4";
-            this.progressBar4.Size = new System.Drawing.Size(248, 22);
-            this.progressBar4.TabIndex = 12;
-            this.progressBar4.Visible = false;
-            // 
-            // progressBar5
-            // 
-            this.progressBar5.Location = new System.Drawing.Point(287, 280);
-            this.progressBar5.Name = "progressBar5";
-            this.progressBar5.Size = new System.Drawing.Size(248, 22);
-            this.progressBar5.TabIndex = 13;
-            this.progressBar5.Visible = false;
-            // 
-            // progressBar6
-            // 
-            this.progressBar6.Location = new System.Drawing.Point(287, 330);
-            this.progressBar6.Name = "progressBar6";
-            this.progressBar6.Size = new System.Drawing.Size(248, 22);
-            this.progressBar6.TabIndex = 14;
-            this.progressBar6.Visible = false;
-            // 
-            // progressBar7
-            // 
-            this.progressBar7.Location = new System.Drawing.Point(287, 380);
-            this.progressBar7.Name = "progressBar7";
-            this.progressBar7.Size = new System.Drawing.Size(248, 22);
-            this.progressBar7.TabIndex = 15;
-            this.progressBar7.Visible = false;
-            // 
-            // progressBar8
-            // 
-            this.progressBar8.Location = new System.Drawing.Point(287, 430);
-            this.progressBar8.Name = "progressBar8";
-            this.progressBar8.Size = new System.Drawing.Size(248, 22);
-            this.progressBar8.TabIndex = 16;
-            this.progressBar8.Visible = false;
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(915, 577);
+            this.ClientSize = new System.Drawing.Size(610, 375);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DownloadButton);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Downloader";
             this.panel1.ResumeLayout(false);
@@ -226,7 +177,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button DownloadButton;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ComboBox comboBox;
         private System.Windows.Forms.CheckBox checkBox6;
@@ -236,13 +187,7 @@
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.ProgressBar progressBar8;
-        private System.Windows.Forms.ProgressBar progressBar7;
-        private System.Windows.Forms.ProgressBar progressBar6;
-        private System.Windows.Forms.ProgressBar progressBar5;
-        private System.Windows.Forms.ProgressBar progressBar4;
-        private System.Windows.Forms.ProgressBar progressBar3;
-        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Downloader/Form1.cs
+++ Downloader/Form1.cs
@@ -1,57 +1,105 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Downloader
 {
-    /// <Links>
-    /// Google Chrome https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B7A1780AD-3F6F-742D-21EB-69989F896ADE%7D%26lang%3Den%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe
-    /// Sublime       https://download.sublimetext.com/Sublime 
-    /// Docker        https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe
-    /// Steam         https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe
-    /// Origin        https://origin-a.akamaihd.net/Origin-Client-Download/origin/live/OriginThinSetup.exe
-    /// BattleNet     https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP&id=2061206683.1565704433
-    /// uTorrent      http://download-hr.utorrent.com/track/stable/endpoint/utorrent/os/windows
-    /// </Links>
-
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
             label.Hide();
-            var test = Enum.GetValues(typeof(ToolsEnum));
 
-            foreach (var tools in test)
+            foreach (var tools in ToolsList())
             {
-                comboBox.Items.Add(tools);
+                comboBox.Items.Add(tools.Key);
             }
             
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Return Title of tools and links to download
+        /// </summary>
+        /// <returns></returns>
+        private Dictionary<string, string> ToolsList()
         {
-            var toolsList = label.Text.Split('\n');
+            var downloadLinks = new Dictionary<string, string>
+            {
+                { "Google Chrome", @"https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B7A1780AD-3F6F-742D-21EB-69989F896ADE%7D%26lang%3Den%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe" },
+                { "Sublime", @"https://download.sublimetext.com/Sublime Text Build 3207 x64 Setup.exe" },
+                { "Docker", @"https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe" },
+                { "Steam", @"https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe" },
+                { "Origin", @"https://origin-a.akamaihd.net/Origin-Client-Download/origin/live/OriginThinSetup.exe" },
+                { "Skype", @"https://go.skype.com/windows.desktop.download" },
+                { "Spotify", @"https://www.spotify.com/int/download/windows/" },
+                { "Visual Studio", @"https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=Professional&rel=16" },
+                { "Tunnel Bear VPN", @"https://tunnelbear.s3.amazonaws.com/downloads/pc/public/TunnelBear-Installer.exe" },
+                { "uTorrent", @"https://www.utorrent.com/intl/ru/downloads/complete/track/stable/os/win" },
+                { "Battle Net", @"https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP&id=244389858.1565729819" }
+            };
 
-            // Download here 
-            //Task.Run(() =>
-            //    {
-            //        if (label.Text.Contains("Docker"))
-            //        {
-            //        }
-            //    });
+            return downloadLinks;
+        }
+
+        private async void Download()
+        {
+            using (var myWebClient = new WebClient())
+            {
+                await Task.Run(() =>
+                {
+                    Action action = () =>
+                     {
+                        foreach (var tool in ToolsList().Where(x => label.Text.Split('\n').Contains(x.Key)))
+                        {
+                            if (label1.Text == string.Empty)
+                            {
+                                label1.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                            }
+
+                            progressBar1.Maximum = 100;
+                            progressBar1.Minimum = 0;
+                            progressBar1.Value++;
+
+                            myWebClient.DownloadFile(tool.Value, label1.Text + "/" + $"{tool.Key}.exe");
+
+                        }
+
+                        MessageBox.Show($"Download finished successful to {label1.Text} path");
+
+                    };
+                    if (InvokeRequired)
+                    {
+                        Invoke(action);
+                    }
+                    else
+                    {
+                        action();
+                    }
+                });
+            }
+        }
+        private  void DownloadButton_Click(object sender, EventArgs e)
+        {
+             Download();
+
         }
 
         private void Panel1_Paint(object sender, PaintEventArgs e)
         {
-
         }
 
         private void CheckBox4_CheckedChanged(object sender, EventArgs e)
         {
-
         }
 
         private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -71,12 +119,10 @@ namespace Downloader
 
         private void Label1_Click(object sender, EventArgs e)
         {
-
         }
 
         private void FolderBrowserDialog1_HelpRequest(object sender, EventArgs e)
         {
-
         }
 
         private void Button3_Click(object sender, EventArgs e)
@@ -88,9 +134,19 @@ namespace Downloader
 
                 if (fldrDlg.ShowDialog() == DialogResult.OK)
                 {
-                    var test = fldrDlg.SelectedPath;
+                    label1.Text = fldrDlg.SelectedPath;
                 }
+
             }
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }

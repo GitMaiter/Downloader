3fd16481c056343802527ab3e0798cfcfa69251c
GitMaiter <mcmaiter@gmail.com>
2019-08-16 18:38:15 +0300

    Add some shit

--- Downloader/Form1.Designer.cs
+++ Downloader/Form1.Designer.cs
@@ -29,94 +29,96 @@
         private void InitializeComponent()
         {
             this.DownloadButton = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.MainPanel = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
             this.label = new System.Windows.Forms.Label();
             this.comboBox = new System.Windows.Forms.ComboBox();
             this.checkBox6 = new System.Windows.Forms.CheckBox();
             this.checkBox5 = new System.Windows.Forms.CheckBox();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.panel1.SuspendLayout();
+            this.Link = new System.Windows.Forms.TextBox();
+            this.Title = new System.Windows.Forms.TextBox();
+            this.ToolsTitle = new System.Windows.Forms.Label();
+            this.LinkLabel = new System.Windows.Forms.Label();
+            this.MainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // DownloadButton
             // 
-            this.DownloadButton.Location = new System.Drawing.Point(235, 322);
-            this.DownloadButton.Margin = new System.Windows.Forms.Padding(2);
+            this.DownloadButton.Location = new System.Drawing.Point(352, 495);
             this.DownloadButton.Name = "DownloadButton";
-            this.DownloadButton.Size = new System.Drawing.Size(129, 45);
+            this.DownloadButton.Size = new System.Drawing.Size(194, 69);
             this.DownloadButton.TabIndex = 1;
             this.DownloadButton.Text = "Download";
             this.DownloadButton.UseVisualStyleBackColor = true;
             this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
             // 
-            // panel1
+            // MainPanel
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.progressBar1);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.label);
-            this.panel1.Controls.Add(this.comboBox);
-            this.panel1.Controls.Add(this.checkBox6);
-            this.panel1.Controls.Add(this.checkBox5);
-            this.panel1.Location = new System.Drawing.Point(8, 8);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(594, 302);
-            this.panel1.TabIndex = 2;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            this.MainPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.MainPanel.Controls.Add(this.LinkLabel);
+            this.MainPanel.Controls.Add(this.ToolsTitle);
+            this.MainPanel.Controls.Add(this.Title);
+            this.MainPanel.Controls.Add(this.Link);
+            this.MainPanel.Controls.Add(this.label1);
+            this.MainPanel.Controls.Add(this.progressBar1);
+            this.MainPanel.Controls.Add(this.button3);
+            this.MainPanel.Controls.Add(this.AddButton);
+            this.MainPanel.Controls.Add(this.label);
+            this.MainPanel.Controls.Add(this.comboBox);
+            this.MainPanel.Location = new System.Drawing.Point(12, 12);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(891, 465);
+            this.MainPanel.TabIndex = 2;
+            this.MainPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(392, 43);
+            this.label1.Location = new System.Drawing.Point(588, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.Size = new System.Drawing.Size(0, 20);
             this.label1.TabIndex = 17;
             this.label1.Click += new System.EventHandler(this.label1_Click_1);
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(191, 52);
-            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.progressBar1.Location = new System.Drawing.Point(286, 80);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(165, 14);
+            this.progressBar1.Size = new System.Drawing.Size(248, 22);
             this.progressBar1.TabIndex = 9;
             this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(392, 12);
-            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Location = new System.Drawing.Point(588, 18);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(157, 25);
+            this.button3.Size = new System.Drawing.Size(264, 38);
             this.button3.TabIndex = 8;
             this.button3.Text = "Select folder for download";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
-            // button2
+            // AddButton
             // 
-            this.button2.Location = new System.Drawing.Point(512, 179);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(56, 21);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "OK";
-            this.button2.UseVisualStyleBackColor = true;
+            this.AddButton.Location = new System.Drawing.Point(768, 217);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(84, 32);
+            this.AddButton.TabIndex = 7;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.Button2_Click);
             // 
             // label
             // 
             this.label.AutoSize = true;
-            this.label.Location = new System.Drawing.Point(25, 48);
-            this.label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label.Location = new System.Drawing.Point(38, 74);
             this.label.Name = "label";
-            this.label.Size = new System.Drawing.Size(0, 13);
+            this.label.Size = new System.Drawing.Size(0, 20);
             this.label.TabIndex = 6;
             this.label.Click += new System.EventHandler(this.Label1_Click);
             // 
@@ -125,69 +127,109 @@
             this.comboBox.DropDownHeight = 150;
             this.comboBox.FormattingEnabled = true;
             this.comboBox.IntegralHeight = false;
-            this.comboBox.ItemHeight = 13;
-            this.comboBox.Location = new System.Drawing.Point(27, 12);
-            this.comboBox.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox.ItemHeight = 20;
+            this.comboBox.Location = new System.Drawing.Point(40, 18);
             this.comboBox.Name = "comboBox";
-            this.comboBox.Size = new System.Drawing.Size(331, 21);
+            this.comboBox.Size = new System.Drawing.Size(494, 28);
             this.comboBox.TabIndex = 5;
             this.comboBox.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
             // 
             // checkBox6
             // 
             this.checkBox6.AutoSize = true;
-            this.checkBox6.Location = new System.Drawing.Point(403, 112);
-            this.checkBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox6.Location = new System.Drawing.Point(600, 495);
             this.checkBox6.Name = "checkBox6";
-            this.checkBox6.Size = new System.Drawing.Size(146, 17);
+            this.checkBox6.Size = new System.Drawing.Size(218, 24);
             this.checkBox6.TabIndex = 4;
             this.checkBox6.Text = "Delete images after install";
             this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.CheckBox6_CheckedChanged);
             // 
             // checkBox5
             // 
             this.checkBox5.AutoSize = true;
-            this.checkBox5.Location = new System.Drawing.Point(403, 159);
-            this.checkBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox5.Location = new System.Drawing.Point(600, 540);
             this.checkBox5.Name = "checkBox5";
-            this.checkBox5.Size = new System.Drawing.Size(126, 17);
+            this.checkBox5.Size = new System.Drawing.Size(186, 24);
             this.checkBox5.TabIndex = 3;
             this.checkBox5.Text = "Install after download";
             this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.CheckBox5_CheckedChanged);
             // 
             // folderBrowserDialog1
             // 
             this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.FolderBrowserDialog1_HelpRequest);
             // 
+            // Link
+            // 
+            this.Link.Location = new System.Drawing.Point(684, 174);
+            this.Link.Name = "Link";
+            this.Link.Size = new System.Drawing.Size(168, 26);
+            this.Link.TabIndex = 18;
+            this.Link.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // Title
+            // 
+            this.Title.Location = new System.Drawing.Point(684, 132);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(167, 26);
+            this.Title.TabIndex = 19;
+            this.Title.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // ToolsTitle
+            // 
+            this.ToolsTitle.AutoSize = true;
+            this.ToolsTitle.Location = new System.Drawing.Point(584, 135);
+            this.ToolsTitle.Name = "ToolsTitle";
+            this.ToolsTitle.Size = new System.Drawing.Size(80, 20);
+            this.ToolsTitle.TabIndex = 20;
+            this.ToolsTitle.Text = "Tools Title";
+            // 
+            // LinkLabel
+            // 
+            this.LinkLabel.AutoSize = true;
+            this.LinkLabel.Location = new System.Drawing.Point(584, 177);
+            this.LinkLabel.Name = "LinkLabel";
+            this.LinkLabel.Size = new System.Drawing.Size(80, 20);
+            this.LinkLabel.TabIndex = 21;
+            this.LinkLabel.Text = "Tools Link";
+            this.LinkLabel.Click += new System.EventHandler(this.LinkLabel_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(610, 375);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(915, 577);
+            this.Controls.Add(this.MainPanel);
             this.Controls.Add(this.DownloadButton);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
             this.Name = "Form1";
             this.Text = "Downloader";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button DownloadButton;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel MainPanel;
         private System.Windows.Forms.ComboBox comboBox;
         private System.Windows.Forms.CheckBox checkBox6;
         private System.Windows.Forms.CheckBox checkBox5;
         private System.Windows.Forms.Label label;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button AddButton;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Title;
+        private System.Windows.Forms.TextBox Link;
+        private System.Windows.Forms.Label LinkLabel;
+        private System.Windows.Forms.Label ToolsTitle;
     }
 }
 
--- Downloader/Form1.cs
+++ Downloader/Form1.cs
@@ -2,12 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Drawing;
-using System.Drawing.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,51 +12,56 @@ namespace Downloader
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Dictionary with Title of tools and links to download
+        /// </summary>
+        /// <returns></returns>
+        private Dictionary<string, string> downloadLinks = new Dictionary<string, string>()
+        {
+            { "Google Chrome", @"https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B7A1780AD-3F6F-742D-21EB-69989F896ADE%7D%26lang%3Den%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe" },
+            { "Sublime", @"https://download.sublimetext.com/Sublime Text Build 3207 x64 Setup.exe" },
+            { "Docker", @"https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe" },
+            { "Steam", @"https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe" },
+            { "Origin", @"https://origin-a.akamaihd.net/Origin-Client-Download/origin/live/OriginThinSetup.exe" },
+            { "Skype", @"https://go.skype.com/windows.desktop.download" },
+            { "Spotify", @"https://www.spotify.com/int/download/windows/" },
+            { "Visual Studio", @"https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=Professional&rel=16" },
+            { "Tunnel Bear VPN", @"https://tunnelbear.s3.amazonaws.com/downloads/pc/public/TunnelBear-Installer.exe" },
+            { "uTorrent", @"https://www.utorrent.com/intl/ru/downloads/complete/track/stable/os/win" },
+            { "Foxit Reader", @"" },
+            { "Battle Net", @"https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP&id=244389858.1565729819" }
+        };
+
+        private WebClient myWebClient = new WebClient();
+
+        private event AsyncCompletedEventHandler DownloadIsCompleted;
+
         public Form1()
         {
             InitializeComponent();
-            label.Hide();
+            UpdateToolsList();
+            DeleteAllDownloadedTools();
+        }
 
-            foreach (var tools in ToolsList())
+        private void UpdateToolsList()
+        {
+            foreach (var tools in downloadLinks)
             {
                 comboBox.Items.Add(tools.Key);
             }
-            
+
         }
 
-        /// <summary>
-        /// Return Title of tools and links to download
-        /// </summary>
-        /// <returns></returns>
-        private Dictionary<string, string> ToolsList()
+        private  void StartDownload()
         {
-            var downloadLinks = new Dictionary<string, string>
+            using (myWebClient)
             {
-                { "Google Chrome", @"https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B7A1780AD-3F6F-742D-21EB-69989F896ADE%7D%26lang%3Den%26browser%3D4%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe" },
-                { "Sublime", @"https://download.sublimetext.com/Sublime Text Build 3207 x64 Setup.exe" },
-                { "Docker", @"https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe" },
-                { "Steam", @"https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe" },
-                { "Origin", @"https://origin-a.akamaihd.net/Origin-Client-Download/origin/live/OriginThinSetup.exe" },
-                { "Skype", @"https://go.skype.com/windows.desktop.download" },
-                { "Spotify", @"https://www.spotify.com/int/download/windows/" },
-                { "Visual Studio", @"https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=Professional&rel=16" },
-                { "Tunnel Bear VPN", @"https://tunnelbear.s3.amazonaws.com/downloads/pc/public/TunnelBear-Installer.exe" },
-                { "uTorrent", @"https://www.utorrent.com/intl/ru/downloads/complete/track/stable/os/win" },
-                { "Battle Net", @"https://www.battle.net/download/getInstallerForGame?os=win&locale=enUS&version=LIVE&gameProgram=BATTLENET_APP&id=244389858.1565729819" }
-            };
 
-            return downloadLinks;
-        }
-
-        private async void Download()
-        {
-            using (var myWebClient = new WebClient())
-            {
-                await Task.Run(() =>
-                {
-                    Action action = () =>
-                     {
-                        foreach (var tool in ToolsList().Where(x => label.Text.Split('\n').Contains(x.Key)))
+                //await Task.Run(() =>
+               // {
+                    //Action action = () =>
+                        //{
+                        foreach (var tool in downloadLinks.Where(x => label.Text.Split('\n').Contains(x.Key)))
                         {
                             if (label1.Text == string.Empty)
                             {
@@ -71,34 +73,41 @@ namespace Downloader
                             progressBar1.Value++;
 
                             myWebClient.DownloadFile(tool.Value, label1.Text + "/" + $"{tool.Key}.exe");
+                           // myWebClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadDataCallback);
+                            // myWebClient.DownloadFileAsync(new Uri(tool.Value), label1.Text + "/" + $"{tool.Key}.exe");
+                             DownloadIsCompleted += new AsyncCompletedEventHandler(DownloadDataCallback);
 
                         }
-
-                        MessageBox.Show($"Download finished successful to {label1.Text} path");
-
-                    };
-                    if (InvokeRequired)
-                    {
-                        Invoke(action);
-                    }
-                    else
-                    {
-                        action();
-                    }
-                });
+                //     };
+                //    if (InvokeRequired)
+                //    {
+                //        Invoke(action);
+                //    }
+                //    else
+                //    {
+                //        action();
+                //    }
+                //});
             }
+        }
+
+        private void DownloadDataCallback(object s, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show($"Download finished successful to {label1.Text} path");
+
         }
         private  void DownloadButton_Click(object sender, EventArgs e)
         {
-             Download();
+
+            StartDownload();
+            if (checkBox5.Checked && DownloadIsCompleted != null)
+            {
+                InstallAllDownloadedTools();
+            }
 
         }
 
         private void Panel1_Paint(object sender, PaintEventArgs e)
-        {
-        }
-
-        private void CheckBox4_CheckedChanged(object sender, EventArgs e)
         {
         }
 
@@ -147,6 +156,72 @@ namespace Downloader
         private void progressBar1_Click(object sender, EventArgs e)
         {
             
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            // TODO: Fix it
+            try
+            {
+                downloadLinks.Add(Title.Text, Link.Text);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+            UpdateToolsList();
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckBox6_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LinkLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InstallAllDownloadedTools()
+        {
+            try
+            {
+                if (DownloadIsCompleted != null)
+                {
+                    using (Process myProcess = new Process())
+                    {
+                        myProcess.StartInfo.UseShellExecute = false;
+                        DownloadIsCompleted -= new AsyncCompletedEventHandler(DownloadDataCallback);
+                        myProcess.StartInfo.FileName = $"{label1.Text}\\{downloadLinks.First().Key}";
+                        myProcess.StartInfo.CreateNoWindow = true;
+                        myProcess.Start();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void DeleteAllDownloadedTools()
+        {
+            File.Delete(@"C:\Users\rkobz\Desktop\Temp");
+        }
+
+        private void CheckBox5_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
